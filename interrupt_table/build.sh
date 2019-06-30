bb3bf430c794a0b0f43a65d54e484f4bd12fbd63
Johann-Tobias Schäg <joto@wlan-l2065.fem.tu-ilmenau.de>
2019-06-30 09:49:57 +0200

    build.log containing memomry map should be auto generated now

--- interrupt_table/build.sh
+++ interrupt_table/build.sh
@@ -3,4 +3,4 @@
 export tesing_with=$(pwd)/..
 export PATH=$PATH:$tesing_with/new_compiler/bin
 sh2-elf-gcc -Wall -nostdlib -T shelf.x main.c -c -o test.o
-sh2-elf-ld -o vmlinux -T shelf.x -L $tesing_with/new_compiler/lib/gcc/sh2-elf/9.1.0 -L $tesing_with/linux/sh/lib -M test.o -nostdlib
+sh2-elf-ld -o vmlinux -T shelf.x -L $tesing_with/new_compiler/lib/gcc/sh2-elf/9.1.0 -L $tesing_with/linux/sh/lib -M test.o -nostdlib > build.log
